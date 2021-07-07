f78427a13bfdc8adbf19413f02ca66bc0937c7d1
Leilani Leach <leilanileach@yahoo.com>
2021-07-06 18:24:56 -0700

    Update styles for TimerButton

--- src/components/Timer/Timer.js
+++ src/components/Timer/Timer.js
@@ -25,9 +25,11 @@ class Timer extends Component {
   render = () => {
     return (
       <div className="timer-container">
-        <TimerButton buttonAction={this.startTimer} buttonValue={'Start'} />
-        <TimerButton buttonAction={this.stopTimer} buttonValue={'Stop'} />
-        <TimerButton buttonAction={this.resetTimer} buttonValue={'Reset'} />
+        <div className="timer-button-container">
+          <TimerButton buttonAction={this.startTimer} buttonValue={'Start'} />
+          <TimerButton buttonAction={this.stopTimer} buttonValue={'Stop'} />
+          <TimerButton buttonAction={this.resetTimer} buttonValue={'Reset'} />
+        </div>
       </div>)
   }
 }
--- src/components/TimerButton/TimerButton.js
+++ src/components/TimerButton/TimerButton.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './TimerButton.css';
 
 const TimerButton = ({ buttonAction, buttonValue }) => (
-  <div className="button-container" />
+  <div className="button-container">
+    <p className="button-value">{buttonValue}</p>
+  </div>
 );
 
 TimerButton.propTypes = {
