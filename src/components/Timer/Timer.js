fefb63274df86bca16f5735eeff408272b2cf561
Leilani Leach <leilanileach@yahoo.com>
2021-07-12 22:44:28 -0700

    Write tests for timer button methods

--- src/components/Timer/Timer.js
+++ src/components/Timer/Timer.js
@@ -1,6 +1,10 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './Timer.css';
 import TimerButton from '../TimerButton/TimerButton';
+
+const Timer = () => {
+
+}
 
 class Timer extends Component {
   constructor(props) {
--- src/components/Timer/Timer.test.js
+++ src/components/Timer/Timer.test.js
@@ -1,18 +1,52 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount, shallow } from 'enzyme';
 import Timer from './Timer';
-// import { describe } from 'yargs';
 
-let container;
-
-beforeEach(() => (container = shallow(<Timer />)))
 describe('Timer', () => {
 
-  it("should render a <div />", () => {
-    expect(container.find("div").length).toBeGreaterThanOrEqual(1)
-  })
+  let container;
 
-  it("should render 3 instances of the TimerButton component", () => {
-    expect(container.find("TimerButton").length).toEqual(3)
-  })
+  beforeEach(() => (container = shallow(<Timer />)));
+
+  it('should render <div />', () => {
+    expect(container.find('div').length).toEqual(3);
+  });
+
+  it('should render instances of the TimerButton component', () => {
+    expect(container.find('TimerButton').length).toBeGreaterThanOrEqual(1);
+  });
+
+});
+
+describe('mounted Timer', () => {
+  let container;
+
+  beforeEach(() => (container = mount(<Timer />)));
+  // TODO what does the mount function do, why use it instead of copying component like other tests?
+
+  it('invokes startTimer when the start button is clicked', () => {
+    const spy = jest.spyOn(container.instance(), 'startTimer');
+    container.instance().forceUpdate();
+    // TODO look up React instance(), forceUpdate(), simulate
+    expect(spy).toHaveBeenCalledTimes(0);
+    container.find('.start-timer').first().simulate('click');
+    expect(spy).toHaveBeenCalledTimes(1);
+  });
+
+  it('invokes stopTimer when the stop button is clicked', () => {
+    const spy = jest.spyOn(container.instance(), 'stopTimer');
+    container.instance().forceUpdate();
+    expect(spy).toHaveBeenCalledTimes(0);
+    container.find('.stop-timer').first().simulate('click');
+    expect(spy).toHaveBeenCalledTimes(1);
+  });
+
+  it('invokes resetTimer when the reset button is clicked', () => {
+    const spy = jest.spyOn(container.instance(), 'resetTimer');
+    container.instance().forceUpdate();
+    expect(spy).toHaveBeenCalledTimes(0);
+    container.find('.reset-timer').first().simulate('click');
+    expect(spy).toHaveBeenCalledTimes(1);
+  });
 })
+
